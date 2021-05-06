b7dcade9afe8dad1c5edb24f19c1fcaa7b721530
Arun Amatya <aaaaa1234500@gmail.com>
2021-05-06 19:07:34 +0545

    added \ to db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        DB::table('users')->insert([
+        \DB::table('users')->insert([
             'name' => "Arun Amatya",
             'email' => 'arun.amatya12345@gmail.com',
             'password' => Hash::make('123456789'),
