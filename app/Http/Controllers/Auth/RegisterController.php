d5b17ce82d52b41bf33df71ea87c789180696d8c
Arun Amatya <aaaaa1234500@gmail.com>
2021-05-08 08:57:24 +0545

    integrated mail on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Notifications\HelloNewUser;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -73,6 +74,7 @@ class RegisterController extends Controller
         ]);
         $user->created_by = $user->id;
         $user->save();
+        $user->notify(new HelloNewUser());
         return $user;
     }
 }
