98912fcd2f75e4cc12240fa7b25915e1d53f7195
Arun Amatya <aaaaa1234500@gmail.com>
2021-06-28 15:57:45 +0545

    test

--- resources/views/super/dashboard.blade.php
+++ resources/views/super/dashboard.blade.php
@@ -875,7 +875,7 @@
                                                                                                 </a>
                                                                                             </div>
                                                                                             <br/>
-                                                                                            <span><b>Duration  : </b> {{$device->logs['step_run_sec']}}</span><br/>
+                                                                                            <span><b>Duration  : </b> {{$device->logs[0]['step_run_sec']}}</span><br/>
                                                                                         </div>
                                                                                         <div><br>
                                                                                             <span><b>Connection :</b></span> <i id="device_connection_status-{{$device->id}}" style="color:green">Connected</i>
