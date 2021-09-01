67ff7fd50d08a89633fede859a1a410cc492b147
Arun Amatya <aaaaa1234500@gmail.com>
2021-09-01 22:41:02 +0545

    all devices data to pull

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -78,7 +78,7 @@ class DataController extends Controller
         $userDevicesIDs = UserDevices::where('user_id',Auth::user()->id)->pluck('device_id');
         //return response()->json($userDevicesIDs);
         $devices = Device::whereIn('id',$userDevicesIDs)->with('latest_log','setpoints','latest_maintenance_critic_acid','latest_maintenance_pre_filter','latest_maintenance_post_filter','latest_maintenance_general_service','device_settings')->get();
-        return response()->json($devices);
+        // return response()->json($devices);
         $today = date(Carbon::now());
         $thirtyOnedays = date(Carbon::now()->subDays(31));
         $previousDay = date(Carbon::now()->subDays(1));
