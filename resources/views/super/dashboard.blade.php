716b2f2dfc0afd01e28fd2f8a1b09a9dbdf0f98b
Arun Amatya <aaaaa1234500@gmail.com>
2021-09-15 23:08:55 +0545

    debug

--- resources/views/super/dashboard.blade.php
+++ resources/views/super/dashboard.blade.php
@@ -2402,7 +2402,8 @@
                         }
                         // calculate cycles left
                         var device_setpoint_CIP_cycles = userDevices.find(device_id =>device_id = view_live_device).CIP_cycles;
-                        //console.log("CIP CYCLES setpoint: "+ device_setpoint_CIP_Cycles)
+                        console.log(userDevices)
+                        console.log("CIP CYCLES setpoint: "+ device_setpoint_CIP_Cycles)
                         var cycles_left = device_setpoint_CIP_cycles - response.cycle;
                         if(cycles_left < 0)
                             cycles_left = 0;
