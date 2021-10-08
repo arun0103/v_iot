ad508b1720627798f0b68453618aa7027347e548
Arun Amatya <aaaaa1234500@gmail.com>
2021-10-08 18:08:50 +0545

    d

--- resources/views/reseller/dashboard.blade.php
+++ resources/views/reseller/dashboard.blade.php
@@ -787,6 +787,30 @@
     border-radius: 50%;
     }
 </style>
+<style>
+    @keyframes placeHolderShimmer{
+        0%{
+            background-position: -468px 0
+        }
+        100%{
+            background-position: 468px 0
+        }
+    }
+    .linear-background {
+        animation-duration: 1s;
+        animation-fill-mode: forwards;
+        animation-iteration-count: infinite;
+        animation-name: placeHolderShimmer;
+        animation-timing-function: linear;
+        background: #f6f7f8;
+        background: linear-gradient(to right, #eeeeee 8%, #dddddd 18%, #eeeeee 33%);
+        background-size: 1000px 104px;
+        /* height: 338px; */
+        position: relative;
+        overflow: hidden;
+        z-index: 10000;
+    }
+</style>
 
 @endsection
 @section('content')
