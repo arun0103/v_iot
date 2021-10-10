b713e0e9aa417bdeb529da95021dcceb3626eb55
Arun Amatya <aaaaa1234500@gmail.com>
2021-10-10 21:29:07 +0545

    user dashboard

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -137,7 +137,7 @@ class DataController extends Controller
     public function refreshUserDashboardData(){
         $userDevicesIDs = UserDevices::where('user_id',Auth::user()->id)->pluck('device_id');
         //return response()->json($userDevicesIDs);
-        $devices = Device::whereIn('id',$userDevicesIDs)->with('latest_log','setpoints','latest_maintenance_critic_acid','latest_maintenance_pre_filter','latest_maintenance_post_filter','latest_maintenance_general_service','device_settings')->get();
+        $devices = Device::whereIn('id',$userDevicesIDs)->with('latest_log','setpoints:device_id,pure_EC_target,volume_unit','latest_maintenance_critic_acid','latest_maintenance_pre_filter','latest_maintenance_post_filter','latest_maintenance_general_service','device_settings')->get();
         // return response()->json($devices);
         $today = date(Carbon::now());
         $thirtyOnedays = date(Carbon::now()->subDays(31));
