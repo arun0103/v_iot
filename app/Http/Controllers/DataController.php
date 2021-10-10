d15ac1372002a72177d3ed0de977e968c8c5461f
Arun Amatya <aaaaa1234500@gmail.com>
2021-10-10 21:17:09 +0545

    d

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -126,7 +126,7 @@ class DataController extends Controller
     public function refreshDashboardRows(){
         $loggedInUser = Auth::user();
         if($loggedInUser->role == "S"){
-            $devices = Device::with('latest_log','setpoints:device_id,pure_EC_target')->get();
+            $devices = Device::with('latest_log:device_id,log_dt,pure_EC_target','setpoints:device_id,pure_EC_target')->get();
         }else{
             $devices = Device::where('reseller_id',$loggedInUser->reseller_id)->with('latest_log','setpoints')->get();
         }
