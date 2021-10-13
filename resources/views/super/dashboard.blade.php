501f409a6199b61b2eca4ce3f33b41153f157e8e
Arun Amatya <aaaaa1234500@gmail.com>
2021-10-13 21:29:38 +0545

    d

--- resources/views/super/dashboard.blade.php
+++ resources/views/super/dashboard.blade.php
@@ -2565,7 +2565,7 @@
         .done(function(response){
             userDevices = response;
         });
-        $('#modal-detail-title').text($("#"+trid).find("td:eq(2)").html())
+        $('#modal-detail-title').text($("#"+trid).find("td:eq(1)").html())
         device_serial = $("#"+trid).find("td:first").html();
 
         //get data from database every 5 seconds
